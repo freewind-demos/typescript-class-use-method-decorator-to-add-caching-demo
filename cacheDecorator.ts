8c50fa311655cc70d60cff197f42921b56511000
Freewind <nowindlee@gmail.com>
2019-11-25 08:28:29 +0800

    add instance name to cache key

--- cacheDecorator.ts
+++ cacheDecorator.ts
@@ -6,7 +6,7 @@ export default function cache() {
   return function (target: Object, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
     descriptor.value = function (...args: any[]) {
-      const key = `${propertyKey.toString()}(${JSON.stringify(args)})`;
+      const key = `${target.constructor.name}.${propertyKey.toString()}(${JSON.stringify(args)})`;
       return cacher.getOrFetch(key, () => originalMethod.apply(this, args));
     };
   }
